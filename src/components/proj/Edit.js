7b400bc5738d2fcff80b4b298e83980fd9c53923
Hyuhng Min Kim <minkin918@gmail.com>
2021-01-17 13:27:58 +0900

    Update: edit - created markdown editor

--- src/components/proj/Edit.js
+++ src/components/proj/Edit.js
@@ -1,15 +1,60 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Redirect } from 'react-router-dom';
+
+import { Prism as SyntaxHighLighter } from 'react-syntax-highlighter';
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import gfm from 'remark-gfm';
+
 import '../../css/Proj.css';
 
-class DocumentBody extends React.Component {
+const Component = ({ value, language }) => {
+    return (
+        <SyntaxHighLighter language={ language ?? null } style={ prism }>
+            { value ?? "" }
+        </SyntaxHighLighter>
+    )
+}
+
+class EditingWindow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {input: ""};
+    }
+
+    handleChange(e) {
+        this.setState({input: e.target.value});
+        this.props.onMDEdit(e.target.value);
+    }
+
+    render() {
+        return (
+            <div className="proj-edit-sur">
+            <textarea className="proj-edit-textarea" value={this.state.input} onChange={(e) => this.handleChange(e)}></textarea>
+            <ReactMarkdown 
+                className="proj-edit-markdown" 
+                children={this.state.input}
+                renderers={{
+                    code:Component,
+                }}
+                plugins={[gfm]}
+            />
+        </div>
+        )
+    }
+}
+
+class Form extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            isUpdated: false,
+            title: "",
+            description: "",
             contents: ""
-        };
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async getDocument() {
@@ -22,9 +67,56 @@ class DocumentBody extends React.Component {
     
         const data = await response.json();
 
-        if (!data.success) console.log(data);
+        this.setState({
+            title: data.title,
+            description: data.description,
+            contents: data.contents
+        });
+    }
 
-        this.setState({contents: data.contents});
+    handleMDChange = value => {
+        this.setState({contents: value});
+    }
+
+    handleSubmit = async e => {
+        console.log(this.state);
+    }
+
+    componentDidMount() {
+        this.getDocument();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="proj-edit-title-sur"><input 
+                    className="proj-edit-title-input" 
+                    type="text" 
+                    value={this.state.title}
+                    onChange={(e)=>this.setState({title: e.target.value})}
+                /></div>
+                <div className="proj-edit-desc-sur"><input 
+                    className="proj-edit-desc-input" 
+                    type="text" 
+                    value={this.state.description}
+                    onChange={(e)=>this.setState({desc: e.target.value})}
+                /></div>
+                <EditingWindow onMDEdit={this.handleMDChange}/>
+                <div className="proj-edit-button-sur">
+                    <button className="proj-edit-submit-button" type="submit">Edit!</button>
+                </div>
+            </form>
+        )
+    }
+}
+
+class DocumentBody extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isUpdated: false,
+        };
     }
 
     async updateDocument() {
@@ -40,19 +132,12 @@ class DocumentBody extends React.Component {
         if (data.success) this.setState({isUpdated: true});
     }
 
-    componentDidMount() {
-        this.getDocument();
-    }
-
     render() {
         const targetUrl = '/p/' + this.props.id.toString();
         return (
             this.state.isUpdated ? 
-            <Redirect to={targetUrl}/>
-            : <form>
-                <input type="textarea"></input>
-                <button onClick={this.updateDocument}>Edit</button>
-            </form>
+            <Redirect to={targetUrl}/> 
+            : <Form id={this.props.id}/>
         )
     }
 }
@@ -60,11 +145,10 @@ class DocumentBody extends React.Component {
 class Edit extends React.Component {
     render() {
         return (
-            <div className="proj-mainframe">
-                <div className="proj-sur-body">
-                    <div className="proj-vertical-fill">
+            <div className="proj-edit-mainframe">
+                <div className="proj-edit-sur-body">
                         <DocumentBody id={this.props.match.params.id}/>
-                    </div>
+                        {/* <EditingWindow /> */}
                 </div>
             </div>
         )
--- src/components/proj/Proj.js
+++ src/components/proj/Proj.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Read from './Entry';
 import Edit from './Edit';
 import Delete from './Delete';
@@ -7,9 +7,11 @@ import Delete from './Delete';
 const Proj = ({match}) => {
     return (
         <>
-            <Route path={`${match.url}/:id`} component={Read} />
-            <Route path={`${match.url}/edit/:id`} component={Edit}/>
-            {/* <Route path={`${match.url}/delete/:id`} component={Delete}/> */}
+            <Switch>
+                <Route path={`${match.url}/edit/:id`} component={Edit}/>
+                <Route path={`${match.url}/:id`} component={Read} />
+                {/* <Route path={`${match.url}/delete/:id`} component={Delete}/> */}
+            </Switch>
         </>
     )
 };
